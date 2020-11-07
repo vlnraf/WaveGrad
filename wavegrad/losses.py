ef6bc59ffd5aa488e1e87212d15b36e684312f30
vlnraf <raffaele.villani17@gmail.com>
2020-11-07 17:10:30 +0100

    update documentation

--- wavegrad/losses.py
+++ wavegrad/losses.py
@@ -17,3 +17,18 @@ def MSE(y, target, derivative=False):
         return np.mean(np.power(y - target, 2))
     else:
         return 2*(target-y)/y.size
+
+
+def MEE(y, target, derivative=False):
+    """
+    Compute the Mean Euclidean Error (MSE) of the predicted example.
+
+    :param y: the predicted outuput
+    :param target: the target output
+    :param derivative: True for compute derivative, False for compute MSE
+    :return: the MEE or it's derivative.
+    """
+    if derivative == False:
+        return np.linalg.norm(y - target)
+    else:
+        return (y - target) / np.linalg.norm(y - target)
