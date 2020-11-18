71ce5f460ce4c958e1ce5e81e4f3bbd09b10287e
vlnraf <raffaele.villani17@gmail.com>
2020-11-18 12:04:04 +0100

    L1 regularizer added

--- wavegrad/regularizer.py
+++ wavegrad/regularizer.py
@@ -52,8 +52,46 @@ class L2(Regularizer):
         loss_penalty_grad = 0
         for layer in params:
             concat = np.concatenate((layer.weights, layer.bias))
-            loss_penalty_grad += np.square(concat).mean()
+            loss_penalty_grad += concat.mean()
 
-        loss_penalty_grad = 0.5 * self.l2 * loss_penalty_grad
+        loss_penalty_grad = self.l2 * loss_penalty_grad
+
+        return loss_penalty_grad
+
+class L1(Regularizer):
+    def __init__(self, l1=0):
+        """
+        Initizalize the lamda component that will penalize weights
+
+        :params l1: the regularization parameter
+        """
+        self.l1 = l1
+
+    def call(self, params):
+        """
+        Calculate the weights decay.
+        :params layers: the layers of the neural network
+        """
+        loss_penalty = 0
+        for layer in params:
+            concat = np.concatenate((layer.weights, layer.bias))
+            loss_penalty += np.absolute(concat).mean()
+
+        loss_penalty = 0.5 * self.l1 * loss_penalty
+            
+        return loss_penalty
+
+    def gradient(self, params):
+        """
+        Calculate the weights decay.
+
+        :params layers: the layers of the neural network
+        """
+        loss_penalty_grad = 0
+        for layer in params:
+            concat = np.concatenate((layer.weights, layer.bias))
+            loss_penalty_grad += np.sign(concat).mean()
+
+        loss_penalty_grad = 0.5 * self.l1 * loss_penalty_grad
 
         return loss_penalty_grad
